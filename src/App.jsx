fc908cde7c198717bc6555612ba79b2daa918eb3
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-07 00:21:47 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import './App.css'
-import Frist from './component/Frist';
 function App() {
-  const [heading, setHeading] = useState("");
-const video = [
-  {
-      name:"Frist video",
-      id : 1,
-      discription : " video 1",
-      link: "Videos/Video.mp4"
-  }
-  ,
-  {
-      name :"second video",
-      id : 2,
-      discription : " video 2",
-      link: "Videos/Video.mp4"
-  }
-  ,
-  {
-       name  :"third video",
-       id : 3,
-       discription : " video 3",
-       link : "Videos/Video.mp4"
-  }
-];
- useEffect(()=>{
-  if(video.length<=0){
-    setHeading("no videos are available");
-  }else{
-    setHeading(" there are "+ video.length +" videos ")
-  }
- },[]);
+  const {data:Data,isloading, error} = useQuery({
+    queryfn:()=>{
+          fetch('https://jsonplaceholder.typicode.com/comments?_limit=10').then((res)=>res.json());
+    },
+    queryKey:['Data']
+  });
+  if(isloading) return <div>loading.....</div>;
+  if(error) return <div><i>error .......</i></div>;
   return (
     <>
-    <h1>{heading}</h1>
-    {video.map((video)=>
-         <Frist key={video.id} videos={video} />
-     )}
+    <div>
+      {
+        Data && Data.map((val)=>{
+          let store =<h2 key={val.id} className="users">
+             {val.id}  
+            {val.email}
+        </h2>
+        return store;
+        })
+      }
+    </div>
     </>
   )
 }
